5586fc03725df97795f9b9bd4b8a694298e54e70
hiaeia <pipedreamer@yeah.net>
2017-03-26 00:53:27 -0700

    add vimrc.sh

---
+++ gitconfig.sh
@@ -0,0 +1,4 @@
+git config --global core.editor vim
+git config --global user.name hiaeia
+git config --global user.email pipedreamer@yeah.net
+git config credential.helper store
---
+++ vimrc.sh
@@ -0,0 +1,1 @@
+echo -e "set ts=4\nset expandtab\nset tags=/usr/include/tags" > ~/.vimrc
