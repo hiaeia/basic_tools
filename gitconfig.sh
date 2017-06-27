73f186acde8545c06e67fa876fba8a0587b39bd4
hiaeia <pipedreamer@yeah.net>
2017-06-27 10:06:10 +0800

    Update gitconfig.sh

--- gitconfig.sh
+++ gitconfig.sh
@@ -1,4 +1,14 @@
 git config --global core.editor vim
+git config --global color.ui auto
+
 git config --global user.name hiaeia
 git config --global user.email pipedreamer@yeah.net
+
+git config --global http.sslVerify false
+git config --global http.postBuffer 512000000
+git config --global http.proxy http://127.0.0.1:8080
+git config --global https.proxy http://127.0.0.1:8080
+
+git config --global push.default matching
+
 git config credential.helper store
