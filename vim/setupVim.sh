d4dedba6be11a3a4ecce1a5c5c1fc8537b905612
hiaeia <pipedreamer@yeah.net>
2017-06-27 17:51:06 +0800

    modified vim install script.

---
+++ vim/setupVim.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+rm -rf ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp vimrc ~/.vimrc
--- vimrc.sh
+++
@@ -1,1 +0,0 @@
-echo -e "set ts=4\nset expandtab\nset tags=/usr/include/tags" > ~/.vimrc
