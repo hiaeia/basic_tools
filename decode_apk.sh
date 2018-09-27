470088852d3940bf84aa0b9b942cb9632da2f522
resources <pipedreamer@yeah.net>
2018-09-27 20:07:39 +0800

    Update decode_apk.sh

--- decode_apk.sh
+++ decode_apk.sh
@@ -1,9 +1,11 @@
 #!/bin/zsh
 target_apk=$1
-
 rm -rf apk smali
 unzip $target_apk "classes.dex" -d apk
+
+#apktool 下载 https://bitbucket.org/iBotPeaches/apktool/downloads
 apktool decode -o smali $target_apk
+#dex2jar 下载 https://github.com/pxb1988/dex2jar
 d2j-dex2jar.sh apk/classes.dex -o classes.jar
 rm -rf apk 
 
